538c1c45b84e11b7443c22dad1224bf03b41e9ef
Olov Lassus <olov.lassus@gmail.com>
2011-03-23 12:01:31 +0100

    annotation-printer moved to plugins

--- src/plugins/annotation-printer.js
+++ src/plugins/annotation-printer.js
@@ -1,8 +1,8 @@
 "use strict"; "use restrict";
 
-var Shaper = Shaper || require("./shaper.js") || Shaper;
-var Annotater = Annotater || require("./annotater.js") || Annotater;
-var Fmt = Fmt || require("./fmt.js") || Fmt;
+var Shaper = Shaper || require("../shaper.js") || Shaper;
+var Annotater = Annotater || require("../annotater.js") || Annotater;
+var Fmt = Fmt || require("../fmt.js") || Fmt;
 
 Annotater(/@[_$a-zA-Z0-9]*/, function(node, match) {
     console.log(Fmt("{0} annotates {1}", match, node));
