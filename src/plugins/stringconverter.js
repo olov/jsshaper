5c7487d7974a1fc38f0f9d9075eb735ffa39fe39
Olov Lassus <olov.lassus@gmail.com>
2011-03-23 14:15:51 +0100

    stringconverter plugin

---
+++ src/plugins/stringconverter.js
@@ -0,0 +1,24 @@
+"use strict"; "use restrict";
+
+var Shaper = Shaper || require("../shaper.js") || Shaper;
+
+// converts ""+expr or expr+"" to String(expr)
+Shaper("stringconverter", function(root) {
+    return Shaper.traverseTree(root, {pre: function(node, ref) {
+        if (node.type !== tkn.PLUS) {
+            return;
+        }
+        var l = node.children[0];
+        var r = node.children[1];
+        var expr = (l.type === tkn.STRING && l.value === "") ? r :
+            (r.type === tkn.STRING && r.value === "") ? l : null;
+        if (expr === null) {
+            return;
+        }
+
+        var call = Shaper.parseExpression("String($)");
+        call.srcs[0] = node.srcs[0];
+        Shaper.replace(call, expr);
+        return ref.set(call);
+    }});
+});
---
+++ tests/test-stringconverter.js
@@ -0,0 +1,6 @@
+"" + 1; // -> String(1);
+(/*pre2*/ 2*3 + ""); // -> (/*pre2*/ String(2*3));
+"" + 1 + 2; // -> String(1) + 2;
+
+"x" + "y"; // not converted
+"x" + y; // not converted
