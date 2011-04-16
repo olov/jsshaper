0c729af2b0ebe7b44c85f50e1a18c332f8459e3f
Olov Lassus <olov.lassus@gmail.com>
2011-04-16 16:16:02 +0200

    asserter plugin also shapes Assert.anything() calls

--- src/plugins/asserter.js
+++ src/plugins/asserter.js
@@ -14,7 +14,10 @@ Shaper("asserter", function(root) {
                 return;
             }
             var callexpr = node.children[0];
-            if (callexpr.type === tkn.IDENTIFIER && (callexpr.value.toLowerCase() === "assert")) {
+            // shape assert(), Assert(), assert.anything() and Assert.anything() calls
+            if (callexpr.type === tkn.IDENTIFIER && callexpr.value.toLowerCase() === "assert" ||
+                callexpr.type === tkn.DOT && callexpr.children[0].type === tkn.IDENTIFIER &&
+                callexpr.children[0].value.toLowerCase() === "assert") {
                 var params = node.children[1].children;
                 var str = Fmt('"{0}, function {1}, file {2}, line {3}"',
                               params[0].getSrc().replace(/"/g, '\\"'),
