078d86469b94eb0cbac37f12a25e5b69614d9fb0
Olov Lassus <olov.lassus@gmail.com>
2011-04-17 21:23:27 +0200

    asserter plugin: simplified code by using declarative matching via Shaper.match

--- src/plugins/asserter.js
+++ src/plugins/asserter.js
@@ -5,19 +5,14 @@ var Fmt = Fmt || require("../fmt.js") || Fmt;
 
 Shaper("asserter", function(root) {
     var fns = [];
+    var callTempl = Shaper.parseExpression("Assert($$)");
+    var dotTempl = Shaper.parseExpression("Assert.$($$)");
     return Shaper.traverseTree(root, {
         pre: function(node, ref) {
             if (node.type === tkn.FUNCTION) {
                 fns.push(node);
             }
-            if (node.type !== tkn.CALL) {
-                return;
-            }
-            var callexpr = node.children[0];
-            // shape assert(), Assert(), assert.anything() and Assert.anything() calls
-            if (callexpr.type === tkn.IDENTIFIER && callexpr.value.toLowerCase() === "assert" ||
-                callexpr.type === tkn.DOT && callexpr.children[0].type === tkn.IDENTIFIER &&
-                callexpr.children[0].value.toLowerCase() === "assert") {
+            if (Shaper.match(callTempl, node) || Shaper.match(dotTempl, node)) {
                 var params = node.children[1].children;
                 var str = Fmt('"{0}, function {1}, file {2}, line {3}"',
                               params[0].getSrc().replace(/"/g, '\\"'),
@@ -25,7 +20,6 @@ Shaper("asserter", function(root) {
                               fns[fns.length - 1].name || "<anonymous>",
                               node.tokenizer.filename,
                               node.lineno);
-
                 Shaper.insertArgument(node, Shaper.parseExpression(str), -1);
             }
         },
