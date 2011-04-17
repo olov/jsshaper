a36ee82978d661e29930f44bad6324793d743742
Olov Lassus <olov.lassus@gmail.com>
2011-04-17 21:14:11 +0200

    Shaper.match, easier to read traverseData

--- src/shaper.js
+++ src/shaper.js
@@ -4,6 +4,7 @@ var require = require || function(f) { load(f); };
 /* global Narcissus, tkn */ (typeof Narcissus === "undefined") && require("./narcissus.js");
 var Fmt = Fmt || require("./fmt.js") || Fmt;
 var Ref = Ref || require("./ref.js") || Ref;
+var Log = Log || require("./log.js") || Log;
 var Assert = Assert || require("./assert.js") || Assert;
 var Comments = Comments || require("./comments.js") || Comments;
 var log = (typeof console !== "undefined") && console.log || print;
@@ -20,42 +21,110 @@ var Shaper = (function() {
     var traverseData = (function() {
         var o = [];
 
-        o[tkn.FUNCTION] = ["body"];
-        o[tkn.IF] = ["condition", "thenPart", "elsePart"];
-        o[tkn.SWITCH] = ["discriminant", "cases"];
-        o[tkn.CASE] = ["caseLabel", "statements"];
-        o[tkn.DEFAULT] = ["statements"];
-        o[tkn.FOR] = ["setup", "condition", "update", "body"];
-        o[tkn.WHILE] = ["condition", "body"];
-        o[tkn.FOR_IN] = [/*"varDecl",*/ "iterator", "object", "body"];
-        o[tkn.DO] = ["body", "condition"];
-        o[tkn.TRY] = ["tryBlock", "catchClauses", "finallyBlock"];
-        o[tkn.CATCH] = ["guard", "block"];
-        o[tkn.THROW] = ["exception"];
-        o[tkn.RETURN] = ["value"];
-        o[tkn.YIELD] = ["value"];
-        o[tkn.GENERATOR] = ["expression", "tail"];
-        o[tkn.WITH] = ["object", "body"];
-        o[tkn.SEMICOLON] = ["expression"];
-        o[tkn.LABEL] = ["statement"];
-        o[tkn.IDENTIFIER] = ["initializer"];
-        o[tkn.GETTER] = ["body"];
-        o[tkn.SETTER] = ["body"];
-        o[tkn.LET_BLOCK] = ["variables", "expression", "block"];
-        o[tkn.ARRAY_COMP] = ["expression", "tail"];
-        o[tkn.COMP_TAIL] = ["children", "guard"]; // children and custom
+        o[tkn.ARRAY_COMP] = [/*expr*/"expression", /*COMP_TAIL*/"tail"];
+        o[tkn.CASE] = [/*expr*/"caseLabel", /*BLOCK*/"statements"];
+        o[tkn.CATCH] = [/*expr*/"guard", /*BLOCK*/"block"];
+        o[tkn.COMP_TAIL] = [/*[FOR_IN]*/"children", /*expr*/"guard"]; // has children but not last
+        o[tkn.DEFAULT] = [/*BLOCK*/"statements"];
+        o[tkn.DO] = [/*stmt*/"body", /*expr*/"condition"];
+        o[tkn.FOR] = [/*expr*/"setup", /*expr*/"condition", /*expr*/"update", /*stmt*/"body"];
+        o[tkn.FOR_IN] = [/*todo "varDecl",*/ /*IDENTIFIER*/"iterator", /*expr*/"object", /*stmt*/"body"];
+        o[tkn.FUNCTION] = [/*SCRIPT*/"body"];
+        o[tkn.GENERATOR] = [/*expr*/"expression", /*COMP_TAIL*/"tail"];
+        o[tkn.GETTER] = [/*SCRIPT*/"body"];
+        o[tkn.IDENTIFIER] = [/*expr*/"initializer"];
+        o[tkn.IF] = [/*expr*/"condition", /*stmt*/"thenPart", /*stmt*/"elsePart"];
+        o[tkn.LABEL] = [/*stmt*/"statement"];
+        o[tkn.LET_BLOCK] = [/*LET*/"variables", /*expr*/"expression", /*BLOCK*/"block"];
+        o[tkn.RETURN] = [/*expr*/"value"];
+        o[tkn.SEMICOLON] = [/*expr*/"expression"];
+        o[tkn.SETTER] = [/*SCRIPT*/"body"];
+        o[tkn.SWITCH] = [/*expr*/"discriminant", /*[CASE|DEFAULT]*/"cases"];
+        o[tkn.THROW] = [/*expr*/"exception"];
+        o[tkn.TRY] = [/*BLOCK*/"tryBlock", /*[CATCH]*/"catchClauses", /*BLOCK*/"finallyBlock"];
+        o[tkn.WHILE] = [/*expr*/"condition", /*stmt*/"body"];
+        o[tkn.WITH] = [/*expr*/"object", /*stmt*/"body"];
+        o[tkn.YIELD] = [/*expr*/"value"];
 
-        var c = [tkn.SCRIPT,
-                 tkn.BLOCK, tkn.LET, tkn.VAR, tkn.CONST, tkn.COMMA, tkn.ASSIGN,
-                 tkn.HOOK, tkn.OR, tkn.AND, tkn.BITWISE_OR, tkn.BITWISE_XOR,
-                 tkn.BITWISE_AND, tkn.EQ, tkn.NE, tkn.STRICT_EQ, tkn.STRICT_NE,
-                 tkn.LT, tkn.LE, tkn.GE, tkn.GT, tkn.IN, tkn.INSTANCEOF,
-                 tkn.LSH, tkn.RSH, tkn.URSH, tkn.PLUS, tkn.MINUS, tkn.MUL,
-                 tkn.DIV, tkn.MOD, tkn.DELETE, tkn.VOID, tkn.TYPEOF,
-                 tkn.NOT, tkn.BITWISE_NOT, tkn.UNARY_PLUS, tkn.UNARY_MINUS,
-                 tkn.INCREMENT, tkn.DECREMENT, tkn.DOT, tkn.INDEX,
-                 tkn.LIST, tkn.CALL, tkn.NEW, tkn.NEW_WITH_ARGS, tkn.ARRAY_INIT,
-                 tkn.OBJECT_INIT, tkn.PROPERTY_INIT, tkn.GROUP];
+/*
+        // TODO these properties aren't nodes but still relevant
+        x[tkn.ASSIGN] = ["assignOp"]; // number ("value" has string representation)
+        x[tkn.INCREMENT] = ["postfix"]; // boolean ("value" is just "++")
+        x[tkn.DECREMENT] = ["postfix"]; // boolean
+        x[tkn.FUNCTION] = ["functionForm", "name", "params"]; // number, string, array of strings
+        x[tkn.FOR_IN] = ["isEach"]; // boolean
+        x[tkn.SWITCH] = ["defaultIndex"]; // number
+        x[tkn.CATCH] = ["varName"]; // string
+        x[tkn.LABEL] = ["label"]; // string
+        x[tkn.IDENTIFIER] = ["value"]; // string, same as "name" when part of VAR 
+        x[tkn.NUMBER] = ["value"]; // number (can differ from srcs)
+        x[tkn.REGEXP] = ["value"]; // string
+        x[tkn.STRING] = ["value"]; // string (can differ from srcs)
+        x[tkn.GETTER] = ["functionForm", "name", "params"]; // same as FUNCTION
+        x[tkn.SETTER] = ["functionForm", "name", "params"]; // same as FUNCTION
+*/
+
+        var c = [
+            /*[stmt]*/
+            tkn.SCRIPT, tkn.BLOCK,
+
+            /*[expr]*/
+            tkn.COMMA,
+
+            /*expr*/
+            tkn.GROUP,
+
+            /*[expr]*/
+            tkn.ARRAY_INIT,
+
+            /*[PROPERTY_INIT]*/
+            tkn.OBJECT_INIT,
+
+            /*IDENTIFIER, expr*/
+            tkn.PROPERTY_INIT,
+
+            /*[IDENTIFIER]*/
+            tkn.LET, tkn.VAR, tkn.CONST,
+
+            /*expr*/
+            tkn.NEW,
+
+            /*expr, LIST*/
+            tkn.NEW_WITH_ARGS,
+
+            /*expr, LIST*/
+            tkn.CALL,
+
+            /*[expr]*/
+            tkn.LIST,
+
+            /*expr, expr, expr (ternary operator)*/
+            tkn.HOOK,
+
+            /*expr, expr (binary operator)*/
+            tkn.PLUS, tkn.MINUS, tkn.MUL, tkn.DIV, tkn.MOD,
+            tkn.LSH, tkn.RSH, tkn.URSH,
+            tkn.OR, tkn.AND,
+            tkn.BITWISE_OR, tkn.BITWISE_XOR, tkn.BITWISE_AND,
+            tkn.EQ, tkn.NE, tkn.STRICT_EQ, tkn.STRICT_NE,
+            tkn.LT, tkn.LE, tkn.GE, tkn.GT,
+            tkn.IN, tkn.INSTANCEOF,
+            tkn.INDEX,
+
+            /*IDENTIFIER|DOT|INDEX, expr (binary operator)*/
+            tkn.ASSIGN,
+
+            /*expr, IDENTIFIER (binary operator)*/
+            tkn.DOT,
+
+            /*IDENTIFIER|DOT|INDEX (unary operator)*/
+            tkn.INCREMENT, tkn.DECREMENT,
+
+            /*expr (unary operator)*/
+            tkn.UNARY_PLUS, tkn.UNARY_MINUS,
+            tkn.NOT, tkn.BITWISE_NOT,
+            tkn.DELETE, tkn.VOID, tkn.TYPEOF
+        ];
 
         // add "children" to all tokens enumerated in c
         for (var i = 0; i < c.length; i++) {
@@ -118,6 +187,95 @@ var Shaper = (function() {
         }
 
         return node;
+    }
+
+    var MISMATCH = 0;
+    var MATCH = 1;
+    var MATCH_REMAINING = 2;
+    function match(t, n, verbose) {
+        if (t && t.type === tkn.SEMICOLON && t.srcs[1] === "" &&
+            t.expression && t.expression.type === tkn.IDENTIFIER &&
+            (t.expression.value === "$" || t.expression.value === "$$")) {
+            return match(t.expression, n, verbose);
+        }
+        if (t && t.type === tkn.IDENTIFIER) {
+            if (t.value === "$") {
+                // ok (template is $ so matches anything)
+                // todo should $ match null/undefined?
+                verbose && Log("match {0} {1}", t, n);
+                return MATCH;
+            }
+            else if (t.value === "$$") {
+                // ok (template is $$ so matches any remaining children)
+                // todo should $$ match null/undefined?
+                verbose && Log("match_remaining {0} {1}", t, n);
+                return MATCH_REMAINING;
+            }
+        }
+        if (!t || !n) {
+            verbose && Log("{2} {0} {1}", t, n, !t === !n ? "match" : "mismatch");
+            return !t === !n ? MATCH : MISMATCH;
+        }
+        if (t.type !== n.type) {
+            // fail (type mismatch)
+            verbose && Log("mismatch {0} {1}", t, n);
+            return MISMATCH;
+        }
+        if (t.type === tkn.IDENTIFIER ||
+            t.type === tkn.NUMBER ||
+            t.type === tkn.REGEXP ||
+            t.type === tkn.STRING) {
+            if (t.value === n.value ||
+                (t.type === tkn.NUMBER && isNaN(t.value) && isNaN(n.value))) {
+                // ok (terminals with matching values)
+                verbose && Log("match {0} {1}", t, n);
+                return MATCH;
+            }
+            else {
+                // fail (terminals with different values)
+                verbose && Log("mismatch {0} {1}", t, n);
+                return MISMATCH;
+            }
+        }
+
+        // traverse descendants
+        var subprops = traverseData[t.type] || [];
+        var res;
+        for (var i = 0; i < subprops.length; i++) {
+            var prop = subprops[i];
+            if (Array.isArray(t[prop])) {
+                for (var j = 0, k = t[prop].length; j < k; j++) {
+                    res = match(t[prop][j], n[prop][j], verbose);
+                    if (res === MISMATCH) {
+                        verbose && Log("mismatch {0} {1}", t[prop][j], n[prop][j]);
+                        return MISMATCH;
+                    }
+                    else if (res === MATCH_REMAINING) {
+                        verbose && Log("match_remaining {0} {1}", t[prop][j], n[prop][j]);
+                        j = n[prop].length;
+                        break;
+                    }
+                }
+                if (j !== n[prop].length) {
+                    verbose && Log("mismatch {0} {1}", t, n);
+                    return MISMATCH;
+                }
+            }
+            else {
+                res = match(t[prop], n[prop], verbose);
+                if (res === MISMATCH) {
+                    verbose && Log("mismatch {0} {1}", t[prop], n[prop]);
+                    return MISMATCH;
+                }
+                else if (res === MATCH_REMAINING) {
+                    // todo verify ok
+                    verbose && Log("match_remaining {0} {1}", t[prop], n[prop]);
+                    break;
+                }
+            }
+        }
+        verbose && Log("match {0} {1}", t, n);
+        return MATCH;
     }
 
     //// mutate nodes
@@ -299,7 +457,7 @@ var Shaper = (function() {
 
         // extend node.end to right to cover trailing comment
         // before: x*y+z /*c*/, after: x*y+z /*c*/
-        //         -----               -----------
+        //             -                   -------
         i = 0;
         try {
             traverseTree(root, {post: function(node, ref) {
@@ -381,6 +539,7 @@ var Shaper = (function() {
 
     shaper.error = error;
     shaper.traverseTree = traverseTree;
+    shaper.match = match;
     shaper.replace = replace;
     shaper.renameIdentifier = renameIdentifier;
     shaper.insertArgument = insertArgument;
