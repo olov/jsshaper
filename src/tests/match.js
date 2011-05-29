2d9c05f4fe2b4b959194927871e87ab5a9537887
Olov Lassus <olov.lassus@gmail.com>
2011-05-29 12:32:04 +0200

    remove wrapper function in match tests

--- src/tests/match.js
+++ src/tests/match.js
@@ -3,34 +3,28 @@ var Shaper = Shaper || require("shaper.js") || Shaper;
 var Log = Log || require("log.js") || Log;
 var Assert = Assert || require("assert.js") || Assert;
 
-function match(template, node, conds) {
-    return Shaper.match(Shaper.parse(template),
-                        Shaper.parse(node),
-                        conds);
-}
+Assert (Shaper.match("$", Shaper.parse("1")));
+Assert (Shaper.match("$", Shaper.parse("1 + x")));
+Assert (Shaper.match("x = [$]", Shaper.parse("x = [1]")));
+Assert (!Shaper.match("x = [$]", Shaper.parse("x = [1, 2]")));
+Assert (Shaper.match("x = [$$]", Shaper.parse("x = [1, 2]")));
+Assert (Shaper.match("[1, 2, 3]", Shaper.parse("[1, 2, 3]")));
+Assert (!Shaper.match("[1, 2]", Shaper.parse("[1, 2, 3]")));
+Assert (!Shaper.match("[1, 2, 3]", Shaper.parse("[1, 2]")));
+Assert (Shaper.match("[$, $$]", Shaper.parse("[1, 2, [3]]")));
+Assert (Shaper.match("[$, $$]", Shaper.parse("[1, 2, [3]]"), {$: {}, $$: {rest: true}})); // default
+Assert (Shaper.match("[_, __]", Shaper.parse("[1, 2, [3]]"), {_: {}, __: {rest: true}})); // choose your wildcards
+Assert (Shaper.match("[1, $$]", Shaper.parse("[1, 2, 3]")));
+Assert (Shaper.match("function f(a, b) { return $; }", Shaper.parse("function f(a, b) { return 1; }")));
+Assert (!Shaper.match("function f(a, b) { return $; }", Shaper.parse("function g(x) { return 1; }")));
+Assert (Shaper.match("if (true) { $ }", Shaper.parse("if (true) { typeof 1; }")));
+Assert (Shaper.match("if (true) { $ }", Shaper.parse("if (true) { debugger; }")));
+Assert (Shaper.match("function f(a, b) { $$ }", Shaper.parse("function f(a, b) { return 1; }")));
+Assert (Shaper.match("$NUM", Shaper.parse("1"), {$NUM: function(n) { return n.type === tkn.NUMBER; }}));
+Assert (Shaper.match("$NUM", Shaper.parse("1"), {$NUM: {type: tkn.NUMBER}}));
+Assert (Shaper.match("$NUM", Shaper.parse("1"), {$NUM: {type: function(t) { return t === tkn.NUMBER; }}}));
 
-Assert(match("$", "1"));
-Assert(match("$", "1 + x"));
-Assert(match("x = [$]", "x = [1]"));
-Assert(!match("x = [$]", "x = [1, 2]"));
-Assert(match("x = [$$]", "x = [1, 2]"));
-Assert(match("[1, 2, 3]", "[1, 2, 3]"));
-Assert(!match("[1, 2]", "[1, 2, 3]"));
-Assert(!match("[1, 2, 3]", "[1, 2]"));
-Assert(match("[$, $$]", "[1, 2, [3]]"));
-Assert(match("[$, $$]", "[1, 2, [3]]", {$: {}, $$: {rest: true}})); // default
-Assert(match("[_, __]", "[1, 2, [3]]", {_: {}, __: {rest: true}})); // choose your wildcards
-Assert(match("[1, $$]", "[1, 2, 3]"));
-Assert(match("function f(a, b) { return $; }", "function f(a, b) { return 1; }"));
-Assert(!match("function f(a, b) { return $; }", "function g(x) { return 1; }"));
-Assert(match("if (true) { $ }", "if (true) { typeof 1; }"));
-Assert(match("if (true) { $ }", "if (true) { debugger; }"));
-Assert(match("function f(a, b) { $$ }", "function f(a, b) { return 1; }"));
-Assert(match("$NUM", "1", {$NUM: function(n) { return n.type === tkn.NUMBER; }}));
-Assert(match("$NUM", "1", {$NUM: {type: tkn.NUMBER}}));
-Assert(match("$NUM", "1", {$NUM: {type: function(t) { return t === tkn.NUMBER; }}}));
-
-Assert(match("[ONE_STR, REST_NUMBERS]", "['one', 2, 3]", {
+Assert (Shaper.match("[ONE_STR, REST_NUMBERS]", Shaper.parse("['one', 2, 3]"), {
     ONE_STR: {type: tkn.STRING},
     REST_NUMBERS: {rest: true, type: tkn.NUMBER}
 }));
@@ -41,7 +35,7 @@ var conds = {
         return t === tkn.NUMBER || t === tkn.STRING;
     }}
 };
-Assert(match("[ONE_STR, REST_NUMBERS_OR_STRINGS]", "['one', 2, 3]", conds));
-Assert(!match("[ONE_STR, REST_NUMBERS_OR_STRINGS]", "['one', 2, null]", conds));
+Assert (Shaper.match("[ONE_STR, REST_NUMBERS_OR_STRINGS]", Shaper.parse("['one', 2, 3]"), conds));
+Assert (!Shaper.match("[ONE_STR, REST_NUMBERS_OR_STRINGS]", Shaper.parse("['one', 2, null]"), conds));
 
 Log("all tests passed");
