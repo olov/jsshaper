b7ce65e05c2ff1b42eb35c3b8a829903e9d3d885
Olov Lassus <olov.lassus@gmail.com>
2011-07-09 23:50:30 +0200

    minmax example: simplified code by using declarative matching via Shaper.match

--- src/examples/minmax/minmax.js
+++ src/examples/minmax/minmax.js
@@ -4,16 +4,21 @@ var Shaper = Shaper || require("shaper.js") || Shaper;
 var Fmt = Fmt || require("fmt.js") || Fmt;
 
 Shaper("minmax", function(root) {
+    var minTempl = Shaper.parse("MIN($, $)");
+    var maxTempl = Shaper.parse("MAX($, $)");
     return Shaper.traverse(root, {pre: function(node, ref) {
-        if (node.type !== tkn.CALL) {
+        var ternary;
+        if (Shaper.match(minTempl, node)) {
+            ternary = Shaper.parse("(($) < ($) ? ($) : ($))");
+        }
+        else if (Shaper.match(maxTempl, node)) {
+            ternary = Shaper.parse("(($) > ($) ? ($) : ($))");
+        }
+        else {
             return;
         }
-        var fn = node.children[0];
         var params = node.children[1].children;
-        if (fn.type === tkn.IDENTIFIER && (fn.value === "MIN" || fn.value === "MAX")) {
-            var ternary = Shaper.parse(Fmt("(($) {0} ($) ? ($) : ($))", fn.value === "MIN" ? "<" : ">"));
-            Shaper.replace(ternary, params[0], params[1], params[0], params[1]);
-            return ref.set(ternary);
-        }
+        Shaper.replace(ternary, params[0], params[1], params[0], params[1]);
+        return ref.set(ternary);
     }});
 });
