3b1e9a4a85849f7c940e1d478fa89be2cd22b868
Olov Lassus <olov.lassus@gmail.com>
2011-07-12 17:40:05 +0200

    add-to-sub example: Shaper.match

--- src/examples/add-to-sub/add-to-sub.js
+++ src/examples/add-to-sub/add-to-sub.js
@@ -3,13 +3,13 @@
 var Shaper = Shaper || require("shaper.js") || Shaper;
 
 Shaper("add-to-sub", function(root) {
+    var templ = Shaper.parse("$ + $");
     return Shaper.traverse(root, {pre: function(node, ref) {
-        if (node.type === tkn.PLUS) {
+        if (Shaper.match(templ, node)) { // same as: if (node.type === tkn.PLUS) {
             var minus = Shaper.parse("$ - -($)");
             Shaper.replace(minus, node.children[0], node.children[1]);
             Shaper.cloneComments(minus, node); // keep PLUS comment
             return ref.set(minus);
         }
-        return undefined;
     }});
 });
