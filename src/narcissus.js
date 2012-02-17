b42c409e5cdd6028c5b0cb98a203d96cef4696da
C. Scott Ananian <cscott@cscott.net>
2012-02-17 18:53:39 -0500

    Minimal Narcissus AMD-compatibility thunk (leaves Narcissus as a global).

--- src/narcissus.js
+++ src/narcissus.js
@@ -1,10 +1,24 @@
-"empty";
+if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
-if (typeof Narcissus === "undefined") {
-    require("jsecma5.js");
-    /* global Narcissus */
-    require("narcissus/lib/jsdefs.js");
-    require("jsmods.js");
-    require("narcissus/lib/jslex.js");
-    require("narcissus/lib/jsparse.js");
-}
+define(['./jsecma5.js',
+        /* global Narcissus */
+        './narcissus/lib/jsdefs.js',
+        './narcissus/lib/jslex.js',
+        './narcissus/lib/jsparse.js'],
+function(_1,_2,_3,_4) {
+            // dependencies are evaluated just for side-effects
+
+    // from former jsmods.js:
+    // module keyword is removed so that it parses like any identifier
+    // (node uses 'module'; for example in the amdefine statement above)
+    delete Narcissus.definitions.tokens.module;
+    delete Narcissus.definitions.keywords.module;
+    delete Narcissus.definitions.tokenIds.module;
+    // jsmods.js used to define tkn as a global; we've added the
+    // shortcut module 'tkn.js' instead.
+
+    // don't use const; it doesn't work in strict mode
+    Narcissus.hostSupportsEvalConst = false;
+
+    return Narcissus;
+});
